b527cc6c6da442eb6910e88745b71d58f5dc68c9
anunay sinha <anusu90@gmail.com>
2021-02-09 05:38:31 +0530

    sessions

--- server.js
+++ server.js
@@ -27,6 +27,7 @@ app.use((req, res, next) => {
     if (allowedOrigin.indexOf(req.headers.origin) != -1) {
         res.header("Access-Control-Allow-Origin", req.headers.origin);
         res.setHeader("Access-Control-Allow-Credentials", true);
+        res.setHeader("Access-Control-Expose-Headers", "Set-Cookie");
         res.setHeader("Access-Control-Allow-Headers", "Content-Type, Set-Cookie");
         res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
     }
